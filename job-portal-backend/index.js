63587bb750672a84f585d92e1bc613246f58b921
emran-binhasan <emranbinhasan.zero@gmail.com>
2024-12-12 15:10:23 +0600

    Setup Database

---
+++ job-portal-backend/index.js
@@ -0,0 +1,42 @@
+const {configDotenv} = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json())
+configDotenv()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.CONNECTION_STRING
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+ 
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get('/', async (req, res) => {
+        res.send('Server is active')
+    })
+    
+  } finally {
+
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port)
